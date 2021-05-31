b8ef4e2923b49f924643a16dfb879caa71eff1b7
Greg Srtlyan <srtlyan.g@yandex.ru>
2021-05-31 11:09:08 +0300

    add nginx

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
